363d60dc398df79e9d771dfbd0d47d3579540b53
Eric <erictakahashi@gmail.com>
2019-02-05 01:42:30 -0200

    Add video controls

---
+++ Assets/DarkEscape/Scripts/Video.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Video : MonoBehaviour {
+
+	public GameObject						videoObject;
+
+	private UnityEngine.Video.VideoPlayer	videoPlayer;
+
+	// Use this for initialization
+	void Start () {
+		videoPlayer = videoObject.GetComponent<UnityEngine.Video.VideoPlayer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void videoPlay () {
+		videoPlayer.Play ();
+	}
+
+	public void videoPause () {
+		videoPlayer.Pause ();
+	}
+
+}
